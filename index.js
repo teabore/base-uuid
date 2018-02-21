124f114b2b94468a29965369f99931c46cd5af4f
Tibor Szanto <teabore@gmail.com>
2018-02-21 23:34:27 +0700

    Node 4 backward compatibility

--- index.js
+++ index.js
@@ -5,22 +5,29 @@ const maxBase = 64;
 
 const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
 const format = /(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/;
-const length = 32;
+const size = 32;
 
-const _encode = (baseN, str) => {
-  const buffer = Buffer.from(str.replace(/-/g, ''), 'hex');
+const pad = (str, size) => str.padStart ? str.padStart(size, '0') : str + ('0').repeat(size - str.length);
+
+const _encode = (baseN, uuid) => {
+  const buffer = Buffer.from(uuid.replace(/-/g, ''), 'hex');
   return baseN.encode(buffer).replace(/^0+/, '');
 };
 
 const _decodePlain = (baseN, str) => {
   const buffer = Buffer.from(baseN.decode(str));
-  return buffer.toString('hex').padStart(length, '0');
+  const uuid = buffer.toString('hex');
+  return pad(uuid, size);
 };
 
 const _decode = (baseN, str) => _decodePlain(baseN, str).replace(format, '$1-$2-$3-$4-$5');
 
-module.exports = (base = maxBase) => {
-  base = Math.max(minBase, Math.min(maxBase, 0 | base));
+module.exports = (base) => {
+  base = 0 | base || maxBase;
+
+  if (base < minBase || base > maxBase) {
+    throw new Error(`Base must be between ${minBase} and ${maxBase}`);
+  }
 
   const baseN = baseX(chars.substr(0, base));
   const encode = (str) => _encode(baseN, str);
